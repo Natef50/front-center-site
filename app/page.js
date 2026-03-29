2cf311728091340ac1b4f7ce980c1cafdfc93d16
Natef50 <nathanabdelsayed@gmail.com>
2026-03-28 19:40:41 -0700

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{fontFamily: 'sans-serif', padding: 20}}>
+      <h1>Front & Center Decor</h1>
+      <p>Custom decor installations</p>
+      <h2>Book a Setup</h2>
+      <form>
+        <input placeholder="Name" /><br/><br/>
+        <input placeholder="Email" /><br/><br/>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
